e57cefabf998cd7d3b805d61f32ad65c0c0f94d7
Tribhuvan321 <113983064+Tribhuvan321@users.noreply.github.com>
2024-04-03 23:41:04 +0530

    minor arrow modifications

--- Assets/it/Scripts/GizmosTest.cs
+++ Assets/it/Scripts/GizmosTest.cs
@@ -17,6 +17,96 @@ public class GizmosTest : MonoBehaviour
         public double y;
         public double z;
     }
+
+    public class Content
+    {
+        public Coordinates Position;
+        public string EventName;
+        public float time;
+    }
+
+    public class JsonFile
+    {
+        public List<Content> jsonFile;
+    }
+
+    public JsonFile myJsonFileList = new JsonFile();
+
+    void Start()
+    {
+        string jsonText = System.IO.File.ReadAllText(filePath);
+        Debug.Log(jsonText);
+
+        var deserialized = JsonConvert.DeserializeObject<JsonFile>(jsonText);
+
+        numOfPoints = deserialized.jsonFile.Count;
+        plotPoints.Capacity = numOfPoints;
+
+        for (int i = 0; i < numOfPoints; i++)
+        {
+            plotPoints.Add(new Vector3((float)deserialized.jsonFile[i].Position.x, (float)deserialized.jsonFile[i].Position.y, (float)deserialized.jsonFile[i].Position.z));
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < plotPoints.Count - 1; i++)
+        {
+            Vector3 currentPoint = plotPoints[i];
+            Vector3 nextPoint = plotPoints[i + 1];
+
+            Vector3 centerPoint = (currentPoint + nextPoint) / 2f;
+
+            Vector3 direction = nextPoint - currentPoint;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(currentPoint, 0.1f);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(currentPoint, nextPoint);
+
+            if(direction != Vector3.zero)
+            {
+                Handles.color = Color.green;
+                Handles.ConeHandleCap(0, centerPoint, Quaternion.LookRotation(direction, Vector3.up), 0.35f, EventType.Repaint);
+            }
+        }
+
+        GUIStyle labelStyle = new GUIStyle();
+        labelStyle.normal.textColor = Color.yellow;
+        labelStyle.fontSize = 22;
+        labelStyle.fontStyle = FontStyle.Bold;
+        labelStyle.alignment = TextAnchor.MiddleCenter;
+
+        for (int i = 0; i < plotPoints.Count; i++)
+        {
+            Handles.Label(plotPoints[i] + new Vector3(0, 0.5f, 0), new GUIContent(i.ToString()), labelStyle);
+        }
+    }
+}
+
+
+/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+
+public class GizmosTest : MonoBehaviour
+{
+    [SerializeField] private List<Vector3> plotPoints = new List<Vector3>();
+    public string filePath;
+    private int numOfPoints;
+
+    public class Coordinates
+    {
+        public double x;
+        public double y;
+        public double z;
+    }
     public class Content
     {
         //public Dictionary<string,Dictionary<string,double>> 
@@ -161,3 +251,5 @@ public class GizmosTest : MonoBehaviour
     //    Gizmos.DrawLineList(points);
     //}
 }
+ 
+ */
